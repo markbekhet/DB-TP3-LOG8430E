4b4a80f8941834265d1b9e59b8476d211e46d530
Mark Bekhet <markonsy72@gmail.com>
2022-11-20 11:27:38 -0500

    Adjust MongoDB docker-compose to be able to run tests
    This change runs the benchmark against mongoDB
    
    Signed-off-by: Mark Bekhet <markonsy72@gmail.com>

--- script.sh
+++ script.sh
@@ -22,7 +22,28 @@ cd YCSB
 ./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../redis/outputRun.txt
 cd ..
 docker-compose -f redis/docker-compose.yml down
-printf "\nFinished benchmarking redis DB \n"
+printf "\nFinished benchmarking redis DB \n\n"
+
+# Run the container for Mongo DB and run the tests
+printf "\nRunning Benchmarks on Mongo DB, results can be found in the redis folder \n\n"
+docker-compose -f Mongo/docker-compose.yml up -d
+cd Mongo
+docker exec -it primary mongosh --eval "rs.initiate({
+ _id: \"myReplicaSet\",
+ members: [
+   {_id: 0, host: \"192.168.5.2:27017\"},
+   {_id: 1, host: \"192.168.5.3:27017\"},
+   {_id: 2, host: \"192.168.5.4:27017\"},
+   {_id: 3, host: \"192.168.5.5:27017\"}
+ ]
+})"
+
+cd ../YCSB
+./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoad.txt
+./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRun.txt
+cd ..
+docker-compose -f Mongo/docker-compose.yml down
+printf "\nFinished benchmarking Mongo DB \n\n"
 
 # Cleaning up everything
 deactivate
