36a4fb25f5ae132ce43e1088818c5c47b7b22298
Mark Bekhet <markonsy72@gmail.com>
2022-11-22 13:34:48 -0500

    Benchmark cassandra done successfully
    Signed-off-by: Mark Bekhet <markonsy72@gmail.com>

--- script.sh
+++ script.sh
@@ -14,7 +14,7 @@ mvn -pl site.ycsb:redis-binding -am clean package
 
 cd ..
 
-# Run the container for redis DB and run the tests
+## Run the container for redis DB and run the tests
 printf "\nRunning Benchmarks on redis DB, results can be found in the redis folder \n\n"
 docker-compose -f redis/docker-compose.yml up --scale redis-master=1 --scale redis-replica=3 -d
 cd YCSB
@@ -24,7 +24,10 @@ cd ..
 docker-compose -f redis/docker-compose.yml down -v
 printf "\nFinished benchmarking redis DB \n\n"
 
- Run the container for Mongo DB and run the tests
+
+##========================================================================##
+
+## Run the container for Mongo DB and run the tests
 printf "\nRunning Benchmarks on Mongo DB, results can be found in the Mongo folder \n\n"
 docker-compose -f Mongo/docker-compose.yml up -d
 cd Mongo
@@ -47,6 +50,47 @@ cd ../YCSB
 cd ..
 docker-compose -f Mongo/docker-compose.yml down -v
 printf "\nFinished benchmarking Mongo DB \n\n"
+
+##========================================================================##
+
+#Run the container for Cassandra DB and run the tests
+printf "\nRunning Benchmarks on Cassandra DB, results can be found in the cassandra folder \n\n"
+docker-compose -f cassandra/docker-compose.yml up -d &
+#Sleep for a minute to make sure the nodes have communicated together
+sleep 120
+# Make sure that all nodes are in the cluster 4 nodes in total
+docker exec -it cassandra1 nodetool status
+cd YCSB
+
+docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create keyspace ycsb WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor':3}"
+
+docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create table ycsb.usertable (
+    y_id varchar primary key,
+    field0 varchar,
+    field1 varchar,
+    field2 varchar,
+    field3 varchar,
+    field4 varchar,
+    field5 varchar,
+    field6 varchar,
+    field7 varchar,
+    field8 varchar,
+    field9 varchar);"
+
+./bin/ycsb load cassandra-cql -s -P workloads/workloada \
+-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+-p "cassandra.password=cassandra" \
+-p "cassandra.username=cassandra" > ../cassandra/outputLoad.txt
+
+./bin/ycsb run cassandra-cql -s -P workloads/workloada \
+-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+-p "cassandra.password=cassandra" \
+-p "cassandra.username=cassandra" > ../cassandra/outputRun.txt
+
+cd ..
+
+docker-compose -f cassandra/docker-compose.yml down -v
+printf "\nFinished benchmarking Cassandra DB \n\n"
 
 # Cleaning up everything
 deactivate
