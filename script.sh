1ebd7baf16498233572a5bb1a648718b62d902c7
Mark Bekhet <markonsy72@gmail.com>
2022-11-21 21:09:43 -0500

    Remove all references to Hbase database
    Signed-off-by: Mark Bekhet <markonsy72@gmail.com>

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ docker-compose -f redis/docker-compose.yml down
 printf "\nFinished benchmarking redis DB \n\n"
 
 # Run the container for Mongo DB and run the tests
-printf "\nRunning Benchmarks on Mongo DB, results can be found in the redis folder \n\n"
+printf "\nRunning Benchmarks on Mongo DB, results can be found in the Mongo folder \n\n"
 docker-compose -f Mongo/docker-compose.yml up -d
 cd Mongo
 docker exec -it primary mongosh --eval "rs.initiate({
@@ -39,8 +39,11 @@ docker exec -it primary mongosh --eval "rs.initiate({
 })"
 
 cd ../YCSB
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoad.txt
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRun.txt
+./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoadAsync.txt
+./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRunAsync.txt
+
+./bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoad.txt
+./bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRun.txt
 cd ..
 docker-compose -f Mongo/docker-compose.yml down
 printf "\nFinished benchmarking Mongo DB \n\n"
