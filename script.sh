a51450ad4e1f98bdfa0a8dd5dc81524b0253da91
Mark Bekhet <markonsy72@gmail.com>
2022-11-22 14:34:27 -0500

    Execute the redis tests for the five different workloads
    Signed-off-by: Mark Bekhet <markonsy72@gmail.com>

--- script.sh
+++ script.sh
@@ -18,8 +18,46 @@ cd ..
 printf "\nRunning Benchmarks on redis DB, results can be found in the redis folder \n\n"
 docker-compose -f redis/docker-compose.yml up --scale redis-master=1 --scale redis-replica=3 -d
 cd YCSB
-./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" > ../redis/outputLoad.txt
-./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true"> ../redis/outputRun.txt
+printf "Loading data worload a \n" > ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "Running test workoad a \n" > ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
+printf "\n##################################################################################\n" >> ../redis/outputLoad.txt 
+printf " Loading data worload b \n" >> ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "\n##################################################################################\n" >> ../redis/outputRun.txt
+printf "Running test workoad b \n" >> ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
+printf "\n##################################################################################\n" >> ../redis/outputLoad.txt 
+printf "Loading data worload c \n" >> ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "\n##################################################################################\n" >> ../redis/outputRun.txt
+printf "Running test workoad c \n" >> ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloadac -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
+printf "\n##################################################################################\n" >> ../redis/outputLoad.txt 
+printf "Loading data worload d \n" >> ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "\n##################################################################################\n" >> ../redis/outputRun.txt
+printf "Running test workoad d \n" >> ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
+printf "\n##################################################################################\n" >> ../redis/outputLoad.txt
+printf "Loading data worload e \n" >> ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloade -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "\n##################################################################################\n" >> ../redis/outputRun.txt
+printf "Running test workoad e \n" >> ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
+printf "\n##################################################################################\n" >> ../redis/outputLoad.txt
+printf "Loading data worload f \n" >> ../redis/outputLoad.txt 
+./bin/ycsb load redis -s -P workloads/workloadf -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputLoad.txt
+printf "\n##################################################################################\n" >> ../redis/outputRun.txt
+printf "Running test workoad f \n" >> ../redis/outputRun.txt
+./bin/ycsb run redis -s -P workloads/workloadf -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../redis/outputRun.txt
+
 cd ..
 docker-compose -f redis/docker-compose.yml down -v
 printf "\nFinished benchmarking redis DB \n\n"
@@ -28,70 +66,70 @@ printf "\nFinished benchmarking redis DB \n\n"
 ##========================================================================##
 
 ## Run the container for Mongo DB and run the tests
-printf "\nRunning Benchmarks on Mongo DB, results can be found in the Mongo folder \n\n"
-docker-compose -f Mongo/docker-compose.yml up -d
-cd Mongo
-docker exec -it primary mongosh --eval "rs.initiate({
- _id: \"myReplicaSet\",
- members: [
-   {_id: 0, host: \"192.168.5.2:27017\"},
-   {_id: 1, host: \"192.168.5.3:27017\"},
-   {_id: 2, host: \"192.168.5.4:27017\"},
-   {_id: 3, host: \"192.168.5.5:27017\"}
- ]
-})"
-
-cd ../YCSB
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoadAsync.txt
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRunAsync.txt
-
-./bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoad.txt
-./bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRun.txt
-cd ..
-docker-compose -f Mongo/docker-compose.yml down -v
-printf "\nFinished benchmarking Mongo DB \n\n"
-
-##========================================================================##
-
-#Run the container for Cassandra DB and run the tests
-printf "\nRunning Benchmarks on Cassandra DB, results can be found in the cassandra folder \n\n"
-docker-compose -f cassandra/docker-compose.yml up -d &
-#Sleep for a minute to make sure the nodes have communicated together
-sleep 120
-# Make sure that all nodes are in the cluster 4 nodes in total
-docker exec -it cassandra1 nodetool status
-cd YCSB
-
-docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create keyspace ycsb WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor':3}"
-
-docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create table ycsb.usertable (
-    y_id varchar primary key,
-    field0 varchar,
-    field1 varchar,
-    field2 varchar,
-    field3 varchar,
-    field4 varchar,
-    field5 varchar,
-    field6 varchar,
-    field7 varchar,
-    field8 varchar,
-    field9 varchar);"
-
-./bin/ycsb load cassandra-cql -s -P workloads/workloada \
--p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
--p "cassandra.password=cassandra" \
--p "cassandra.username=cassandra" > ../cassandra/outputLoad.txt
-
-./bin/ycsb run cassandra-cql -s -P workloads/workloada \
--p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
--p "cassandra.password=cassandra" \
--p "cassandra.username=cassandra" > ../cassandra/outputRun.txt
-
-cd ..
-
-docker-compose -f cassandra/docker-compose.yml down -v
-printf "\nFinished benchmarking Cassandra DB \n\n"
-
+#printf "\nRunning Benchmarks on Mongo DB, results can be found in the Mongo folder \n\n"
+#docker-compose -f Mongo/docker-compose.yml up -d
+#cd Mongo
+#docker exec -it primary mongosh --eval "rs.initiate({
+# _id: \"myReplicaSet\",
+# members: [
+#   {_id: 0, host: \"192.168.5.2:27017\"},
+#   {_id: 1, host: \"192.168.5.3:27017\"},
+#   {_id: 2, host: \"192.168.5.4:27017\"},
+#   {_id: 3, host: \"192.168.5.5:27017\"}
+# ]
+#})"
+#
+#cd ../YCSB
+#./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoadAsync.txt
+#./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRunAsync.txt
+#
+#./bin/ycsb load mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputLoad.txt
+#./bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url=mongodb://192.168.5.2:27017/ycsb?w=0 > ../Mongo/outputRun.txt
+#cd ..
+#docker-compose -f Mongo/docker-compose.yml down -v
+#printf "\nFinished benchmarking Mongo DB \n\n"
+#
+###========================================================================##
+#
+##Run the container for Cassandra DB and run the tests
+#printf "\nRunning Benchmarks on Cassandra DB, results can be found in the cassandra folder \n\n"
+#docker-compose -f cassandra/docker-compose.yml up -d &
+##Sleep for a minute to make sure the nodes have communicated together
+#sleep 120
+## Make sure that all nodes are in the cluster 4 nodes in total
+#docker exec -it cassandra1 nodetool status
+#cd YCSB
+#
+#docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create keyspace ycsb WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor':3}"
+#
+#docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create table ycsb.usertable (
+#    y_id varchar primary key,
+#    field0 varchar,
+#    field1 varchar,
+#    field2 varchar,
+#    field3 varchar,
+#    field4 varchar,
+#    field5 varchar,
+#    field6 varchar,
+#    field7 varchar,
+#    field8 varchar,
+#    field9 varchar);"
+#
+#./bin/ycsb load cassandra-cql -s -P workloads/workloada \
+#-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+#-p "cassandra.password=cassandra" \
+#-p "cassandra.username=cassandra" > ../cassandra/outputLoad.txt
+#
+#./bin/ycsb run cassandra-cql -s -P workloads/workloada \
+#-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+#-p "cassandra.password=cassandra" \
+#-p "cassandra.username=cassandra" > ../cassandra/outputRun.txt
+#
+#cd ..
+#
+#docker-compose -f cassandra/docker-compose.yml down -v
+#printf "\nFinished benchmarking Cassandra DB \n\n"
+#
 # Cleaning up everything
 deactivate
 rm -rf YCSB
